6b3bf7c00820cc07d2e4a958bcdeeddfa8fc8f71
6NewUser6 <1642258843@qq.com>
2025-10-18 08:38:53 +0800

    feat:添加数据库构建，重构启动类位置

--- backend/StreetCatCareCommunity/src/main/java/com/streetCat/StreetCatCareCommunityApplication.java
+++ backend/StreetCatCareCommunity/src/main/java/com/streetCat/StreetCatCareCommunityApplication.java
@@ -1,13 +1,13 @@
-package org.example.streetcatcarecommunity;
+package com.streetCat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.streetCat.dao")   // 指定 dao 位置
 public class StreetCatCareCommunityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StreetCatCareCommunityApplication.class, args);
     }
-
 }
