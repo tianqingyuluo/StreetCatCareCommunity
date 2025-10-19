a8a2c1b31f857de4b53cf7b5e8409ea2103149c5
6NewUser6 <1642258843@qq.com>
2025-10-19 21:50:42 +0800

    feat:完成了用户登录/注册内容

--- backend/StreetCatCareCommunity/src/main/java/com/streetCat/controller/AuthController.java
+++ backend/StreetCatCareCommunity/src/main/java/com/streetCat/controller/AuthController.java
@@ -1,4 +1,53 @@
 package com.streetCat.controller;
 
+import com.streetCat.service.AuthService;
+import com.streetCat.utils.WxApiUtil;
+import com.streetCat.vo.response.UserLoginResponse;
+import com.streetCat.vo.response.WxSessionResp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 用户相关接口
+ * @author your-name
+ */
+@RestController
+@Tag(name = "用户模块")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @PatchMapping("/user/me")
+    @Operation(summary = "修改个人资料（字段可空）")
+    public Object updateMe(@RequestBody Object dto) {
+        // TODO
+        return null;
+    }
+
+    @PostMapping("/auth/login-wechat")
+    @Operation(summary = "微信openid一键登录/注册")
+    public ResponseEntity<UserLoginResponse> loginWechat(@RequestBody String code) {
+        UserLoginResponse resp = authService.loginWechat(code);
+        return ResponseEntity.ok(resp);
+    }
+
+    @PutMapping("/user/location")
+    @Operation(summary = "更新常驻地理位置")
+    public Object updateLocation(@RequestBody Object dto) {
+        // TODO
+        return null;
+    }
+
+    @GetMapping("/user/{userid}")
+    @Operation(summary = "查看某个id对应的用户（小程序端）")
+    public Object getUser(@PathVariable String userid) {
+        // TODO
+        return null;
+    }
 }
--- backend/StreetCatCareCommunity/src/main/java/com/streetCat/service/impl/AuthServiceImpl.java
+++ backend/StreetCatCareCommunity/src/main/java/com/streetCat/service/impl/AuthServiceImpl.java
@@ -1,4 +1,54 @@
 package com.streetCat.service.impl;
 
-public class AuthServiceImpl {
+import com.streetCat.dao.UserMapper;
+import com.streetCat.pojo.User;
+import com.streetCat.service.AuthService;
+import com.streetCat.utils.JwtUtil;
+import com.streetCat.utils.RandomUtil;
+import com.streetCat.utils.WxApiUtil;
+import com.streetCat.vo.response.UserLoginResponse;
+import com.streetCat.vo.response.WxSessionResp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final UserMapper userMapper;
+    public UserLoginResponse loginWechat(String code) {
+        // 1. 拿微信 session
+        WxSessionResp wx = WxApiUtil.code2session(code);
+        String openid = wx.getOpenid();
+        String unionid = wx.getUnionid();
+        // 2. 根据 openid 查库
+        User user = userMapper.selectByOpenid(openid);
+
+        String type;
+        if (user == null) {
+            // 3. 注册
+            Long uid = RandomUtil.nextId();                 // 程序生成ID
+            String nickname = RandomUtil.randomNickname();  // 随机昵称
+
+            // 入库：只插核心字段
+            userMapper.insertUser(uid, openid, unionid, nickname);
+            user = new User();
+            user.setId(String.valueOf(uid));
+            user.setNickname(nickname);
+            type = "REGISTER";
+        } else {
+            // 4. 登录
+            type = "LOGIN";
+        }
+
+        // 5. 生成自己系统的 token
+        String accessToken = JwtUtil.create(user.getId());
+        UserLoginResponse resp = new UserLoginResponse();
+
+        resp.setType(type);
+        resp.setAccessToken(accessToken);
+        resp.setExpiresIn(7200);
+        resp.setUser(user);
+        return resp;
+    }
+
 }
